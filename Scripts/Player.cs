0def95e0fa3629d468b53202d51f78dfcfb07a07
hellory4n <hellory4n@gmail.com>
2023-06-20 13:02:08 -0400

    yes.

---
+++ Scripts/Player.cs
@@ -0,0 +1,13 @@
+using Godot;
+using System;
+
+public class Player : RigidBody2D {
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        GD.Print("\"this game sucks\" - joe biden, 2023");
+    }
+}
