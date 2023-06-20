dc2899435faa103f82ea864f80ed162b3283abc2
hellory4n <hellory4n@gmail.com>
2023-06-20 14:27:30 -0400

    nuiop´[

---
+++ Scripts/Bus.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public class Bus : RigidBody2D {
+    public override void _Process(float delta) {
+        LinearVelocity = new Vector2(500, LinearVelocity.y);
+        base._Process(delta);
+    }
+}
