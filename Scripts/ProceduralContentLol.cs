ca7a9e857e5fc17e1adb25f7b513ab92a0281355
hellory4n <hellory4n@gmail.com>
2023-07-28 21:30:58 -0400

    utes

--- Scripts/ProceduralContentLol.cs
+++ Scripts/ProceduralContentLol.cs
@@ -3,14 +3,15 @@ using System;
 
 public class ProceduralContentLol : Timer {
     Vector2 GroundPosition = new Vector2(13200, 467);
+    Vector2 TreePosition = new Vector2(15173, 247);
     PackedScene Ground;
-    PackedScene Trees;
-    PackedScene Sky;
+    Texture Trees;
 
     public override void _Ready() {
         base._Ready();
         this.Connect("timeout", this, nameof(ContentCreator));
         Ground = ResourceLoader.Load<PackedScene>("res://Scenes/GroundOmgomgomgogm.tscn");
+        Trees = ResourceLoader.Load<Texture>("res://Assets/Forest.png");
     }
 
     public void ContentCreator() {
@@ -20,6 +21,16 @@ public class ProceduralContentLol : Timer {
             theGround.Position = GroundPosition;
             GetParent().AddChild(theGround);
             GroundPosition = new Vector2(GroundPosition.x+1500, 467);
+        }
+
+        // more trees :))))
+        for (int i = 0; i < 2; i++) {
+            Sprite stupid = new Sprite();
+            stupid.Texture = Trees;
+            stupid.Centered = false;
+            stupid.Position = TreePosition;
+            GetParent().AddChild(stupid);
+            TreePosition = new Vector2(TreePosition.x+9560, 247);
         }
     }
 }
