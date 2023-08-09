b1a6847bb8041f68473ce197a6bcf104865c0678
hellory4n <hellory4n@gmail.com>
2023-08-09 16:13:00 -0400

    funni

--- Scripts/FuckOff.cs
+++ Scripts/FuckOff.cs
@@ -2,6 +2,17 @@ using Godot;
 using System;
 
 public class FuckOff : Sprite {
+    public override void _Ready() {
+        base._Ready();
+        // the ending has a different song
+        GetNode<AudioStreamPlayer>("/root/Menu/Moosic").QueueFree();
+        AudioStreamPlayer shit = new AudioStreamPlayer {
+            Autoplay = true,
+            Stream = ResourceLoader.Load<AudioStreamMP3>("res://Assets/shitending.mp3")
+        };
+        GetTree().Root.AddChild(shit);
+    }
+
     public override void _Process(float delta) {
         RotationDegrees += 0.05f;
         base._Process(delta);
